a8bb864dbb3284d051e97f6ba8f52d984738c886
Sumaya <sumaya.mohamedahmed@student.ap.be>
2023-11-06 21:31:39 +0100

    Cleaning the code by the project page

--- src/pages/projects.js
+++ src/pages/projects.js
@@ -5,19 +5,25 @@ import styles from "./projects.module.css";
 export const Projects = () => {
     return (
         <Layout>
-                <div className={styles.projectCardsContainer}>
-                    <div className={styles.firstProject}>
-                        <h3 className={styles.title}>Personal project</h3>
-                        <div className={styles.linkContainer}><p>Taverna Scheldeland</p><a a href="https://scheldeland-mwokpu3zf-sumaya32.vercel.app/" target="blank">https://tavernascheldeland.be</a></div>
+            <div className={styles.projectCardsContainer}>
+                <div className={styles.firstProject}>
+                    <h3 className={styles.title}>Personal project</h3>
+                    <div className={styles.projecLinkContainer}>
+                        <p>Taverna Scheldeland</p>
+                        <a a href="https://scheldeland-mwokpu3zf-sumaya32.vercel.app/" target="blank">https://tavernascheldeland.be</a>
                     </div>
-
-                    <div className={styles.secondProject}>
-                        <h3 className={styles.title}>School Project</h3>
-                        <div  className={styles.linkContainer}><p>Juba Cocktails</p><a href="https://juba-cocktails.vercel.app/" target="blank">https://juba-cocktails.vercel.app/</a></div>
-                    </div>
-
                 </div>
-                </Layout>
+
+                <div className={styles.secondProject}>
+                    <h3 className={styles.title}>School Project</h3>
+                    <div className={styles.projecLinkContainer}>
+                        <p>Juba Cocktails</p>
+                        <a href="https://juba-cocktails.vercel.app/" target="blank">https://juba-cocktails.vercel.app/</a>
+                    </div>
+                </div>
+
+            </div>
+        </Layout>
     );
 }
 
